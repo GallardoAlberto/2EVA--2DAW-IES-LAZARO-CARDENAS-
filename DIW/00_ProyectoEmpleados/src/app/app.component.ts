e09d19fa07c68a96b21c0ffc800d523fa0ae97d3
GallardoAlberto <gallardo.alb.1998@gmail.com>
2022-11-28 18:01:33 +0100

    ejercicio empleados al dia

--- DIW/00_ProyectoEmpleados/src/app/app.component.ts
+++ DIW/00_ProyectoEmpleados/src/app/app.component.ts
@@ -11,16 +11,31 @@ export class AppComponent {
 
   salario = 0;
   cont = 3;
-
+  parrafo: any;
+  mensaje : any;
+  datos : any;
+  
   mariaBeltran = new Empleado("María", "Beltrán", "Directora", "3000");
   martaGonzalez = new Empleado("Marta", "González", "Coordinadora", "2000");
   pedroPerez = new Empleado("Pedro", "Pérez", "Botones","1500"); 
 
   empleados : Array<Empleado> = new Array(this.mariaBeltran,this.martaGonzalez,this.pedroPerez);
-  
+  apellidosEmpleados : any = [ this.mariaBeltran.apellido, this.martaGonzalez.apellido, this.pedroPerez.apellido];
+
   crearEmpleado(nombre : string, apellido : string, cargo : string, salario : string) {
     let newEmpleado = new Empleado(nombre, apellido, cargo, salario);
-    this.empleados.push(newEmpleado);
+    
+    if(this.apellidosEmpleados.includes(apellido) == true){
+      alert("Empleado con el mismo apellido, inserta otro");
+    }else{
+      this.empleados.push(newEmpleado);
+      this.apellidosEmpleados.push(apellido);
+    }
+
   }
+  
+
+
+  
   
 }
